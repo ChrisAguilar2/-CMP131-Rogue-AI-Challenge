ba9730418b2584d15ecc86cfdc5c11c3cb0332f4
aguilar.christoph520 <aguilar.christoph520@student.ccm.edu>
2026-09-24 11:16:50 -0400

    complete rogue ai challenge

--- rogue_ai.py
+++ rogue_ai.py
@@ -9,14 +9,28 @@ print("========================================")
 
 # LEVEL 1 - TEMPERATURE DIAGNOSTIC
 # Ask for the system temperature and make the required decision.
-
+Temperature = int(input("What is your temperature?"))
+if Temperature >= 100:
+    print("WARNING: SYSTEM OVERHEATING")
+else:
+    print("Temperature Normal")
 
 # LEVEL 2 - POWER DIAGNOSTIC
 # Ask for the battery percentage and make the required decision.
-
+Battery = int(input("What is your battery percentage?"))
+if Battery <= 20:
+    print("LOW POWER")
+else:
+    print("Power Normal")
 
 # LEVEL 3 - SECURITY DIAGNOSTIC
 # Ask for the security status and make the required decision.
+security = input("What is your security status?")
+
+if security == "danger":
+    print("SHUTDOWN REQUIERED")
+else:
+    print("System Secure")
 
 
 print("========================================")
